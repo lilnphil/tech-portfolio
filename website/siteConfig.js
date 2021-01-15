41aa2fe6bda8255fd8fb361bf31764dcc5c5ea24
lilnphil <71635462+lilnphil@users.noreply.github.com>
2021-01-15 13:49:18 -0600

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Lillian Ahles', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    tagline: 'Continuously improving, aspiring technologist, begginer.',
     profilePhoto: 'img/profile.png',
     url: 'https://lilnphil.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
